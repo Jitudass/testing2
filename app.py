2c10bcbb33559f4554f9988da9384f5609dffdc1
Jitudass <jitu007jd@gmail.com>
2023-08-01 23:03:56 +0530

    Developer story

--- app.py
+++ app.py
@@ -1,16 +1,21 @@
-<<<<<<< HEAD
-def addition(a,b):
+## addition code
+def addition(a,b,c):
     print(a+b+c)
     return(a+b)
 
 def multiplication(a,b):
     return(a*b)
 
-=======
+
 ## addition code
 def addition(a,b,c):
     return(a+b+c)
 
 def multiplication(a,b):
     return(a*b)
->>>>>>> 6451d8d7e64412c25343738405ca726445d4191d
+
+
+def division(a,b):
+    return a/b
+
+    
